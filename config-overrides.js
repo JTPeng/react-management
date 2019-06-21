0198f19aca604259579563bc7a60a05ae2a10210
JTPeng <404502753@qq.com>
2019-06-21 11:45:34 +0800

    '删除不必要的文件'

---
+++ config-overrides.js
@@ -0,0 +1,17 @@
+/**
+ * Created by JTPeng on 2019-06-21 11:28.
+ * Description：antd组件配置
+ */
+const { override, fixBabelImports, addLessLoader } = require('customize-cra');
+
+module.exports = override(
+  fixBabelImports('import', {
+    libraryName: 'antd',
+    libraryDirectory: 'es',
+    style: true,
+  }),
+  addLessLoader({
+    javascriptEnabled: true,
+    modifyVars: { '@primary-color': '#1DA57A' },
+  }),
+);
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+/**
+ * Created by JTPeng on 2019-06-21 11:25.
+ * Description：
+ */
+import React,{ Component } from 'react';
+
+import { Button } from 'antd';
+
+export default class App extends Component{
+  render() {
+    return(
+      <Button type="primary">按钮</Button>
+    )
+  }
+}
